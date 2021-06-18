155e7e3d5eada0c939e9e185d7bf0633b8085731
jeffreysford2 <jeffreysford2@gmail.com>
2021-06-18 08:02:37 -0400

    added event listener to hide pop up

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 import LocationInfoBox from './LocationInfoBox'
@@ -7,12 +7,28 @@ const API_KEY = process.env.REACT_APP_API_KEY
 const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
+    useEffect(() => {
+        const listener = e => {
+            if (e.key === 'Escape' || e.target.nodeName === 'DIV') {
+                setLocationInfo(null)
+            }
+        };
+        window.addEventListener('keydown', listener);
+        window.addEventListener('click', listener)
+        return () => {
+            window.removeEventListener('keydown', listener);
+            window.removeEventListener('click', listener)
+        }
+    }, [])
+
     const markers = eventData.map(ev => {
         if (ev.categories[0].id === 8 || ev.categories[0].id === 15) {
             return <LocationMarker key={ev.id} eventType={ev.categories[0].id} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title, eventType: ev.categories[0].title })} />
         }
         return null
     })
+
+
 
     return (
         <div className="map">

7dab9afa5ea9aedfbae5eb7a6071e0ad011318a7
jeffreysford2 <jeffreysford2@gmail.com>
2021-07-13 21:30:16 -0400

    updated .env

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require("mongoose");
+require('dotenv').config()
+const key = process.env.MONGO_KEY
+const mongoURL = process.env.MONGODB_URI
+
 
 app.use(cors());
 app.use(express.json());
 
-const url = "mongodb+srv://jeffreysford2:2.PPsx5g@ehihS$@cluster0.yq5ke.mongodb.net/nasaDB?retryWrites=true&w=majority"
+const url = `mongodb+srv://jeffreysford2:${key}@cluster0.yq5ke.mongodb.net/nasaDB?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -25,6 +29,6 @@ db.once("open", () => {
 
 app.use("/", require("./routes/eventRoute"));
 
-app.listen(3001, function () {
+app.listen(process.env.PORT || 3001, function () {
     console.log("express server is running on port 3001")
 })
